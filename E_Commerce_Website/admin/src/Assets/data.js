37c7e6bfc6c4e8b6d0740969f55dae4976dbdc67
Muhammad Hassan Ghauri <94550154+HassanGhauri@users.noreply.github.com>
2024-07-23 16:09:01 +0500

    Add files via upload

---
+++ E_Commerce_Website/admin/src/Assets/data.js
@@ -0,0 +1,36 @@
+import Pic1 from '../Assets/Pic1.jpg'
+import Pic2 from '../Assets/Pic2.jpg'
+import Pic3 from '../Assets/Pic3.jpg'
+import Pic4 from '../Assets/Pic4.jpg'
+
+let data_product=[
+    {
+        id:1,
+        name:"blue and white full sleeve shirt with blue jeans",
+        image:Pic1,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:2,
+        name:"blue and white full sleeve shirt with blue jeans",
+        image:Pic2,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:3,
+        name:"blue and white full sleeve shirt with blue jeans",
+        image:Pic3,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:4,
+        name:"blue and white full sleeve shirt with blue jeans",
+        image:Pic4,
+        new_price:50.00,
+        old_price:80.00
+    }
+]
+export default data_product;
---
+++ E_Commerce_Website/admin/src/Assets/new_collection.js
@@ -0,0 +1,69 @@
+import Pic5 from '../Assets/Pic5.jpg'
+import Pic6 from '../Assets/Pic6.jpg'
+import Pic7 from '../Assets/Pic7.jpg'
+import Pic8 from '../Assets/Pic8.jpg'
+import product2 from '../Assets/product2.jpg'
+import product3 from '../Assets/product3.jpg'
+import product4 from '../Assets/product4.jpg'
+import product5 from '../Assets/product5.jpg'
+
+
+let new_collection=[
+    {
+        id:1,
+        name:"blue and white full sleeve shirt with blue jeans",
+        image:Pic5,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:2,
+        name:"blue and white full sleeve shirt with blue jeans",
+        image:Pic6,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:3,
+        name:"blue and white full sleeve shirt with blue jeans",
+        image:Pic7,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:4,
+        name:"blue and white full sleeve shirt with blue jeans",
+        image:Pic8,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:5,
+        name:"blue and white full sleeve shirt with blue jeans",
+        image:product2,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:6,
+        name:"blue and white full sleeve shirt with blue jeans",
+        image:product3,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:7,
+        name:"blue and white full sleeve shirt with blue jeans",
+        image:product4,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:8,
+        name:"blue and white full sleeve shirt with blue jeans",
+        image:product5,
+        new_price:50.00,
+        old_price:80.00
+    }
+]
+export default new_collection;
