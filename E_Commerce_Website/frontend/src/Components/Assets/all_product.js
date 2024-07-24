76b62821f7b5ed0d4294b8d2ff5adcc3b1f2c9da
Muhammad Hassan Ghauri <94550154+HassanGhauri@users.noreply.github.com>
2024-07-24 21:31:23 +0500

    Add files via upload

---
+++ E_Commerce_Website/frontend/src/Components/Assets/all_product.js
@@ -0,0 +1,363 @@
+import Pic1 from '../Assets/Pic1.jpg'
+import Pic2 from '../Assets/Pic2.jpg'
+import Pic3 from '../Assets/Pic3.jpg'
+import Pic4 from '../Assets/Pic4.jpg'
+import Pic5 from '../Assets/Pic5.jpg'
+import Pic6 from '../Assets/Pic6.jpg'
+import Pic7 from '../Assets/Pic7.jpg'
+import Pic8 from '../Assets/Pic8.jpg'
+import product2 from '../Assets/product2.jpg'
+import product3 from '../Assets/product3.jpg'
+import product4 from '../Assets/product4.jpg'
+import product5 from '../Assets/product5.jpg'
+import product6 from '../Assets/product6.jpg'
+import product7 from '../Assets/product7.jpg'
+import product8 from '../Assets/product8.jpg'
+import product9 from '../Assets/product9.jpg'
+import product10 from '../Assets/product10.jpg'
+import product11 from '../Assets/product11.jpg'
+import product12 from '../Assets/product12.jpg'
+import product13 from '../Assets/product13.jpg'
+import product14 from '../Assets/product14.jpg'
+import product15 from '../Assets/product15.jpg'
+import product16 from '../Assets/product16.jpg'
+import product17 from '../Assets/product17.jpg'
+import product18 from '../Assets/product18.jpg'
+import product19 from '../Assets/product19.jpg'
+import product20 from '../Assets/product20.jpg'
+import product21 from '../Assets/product21.jpg'
+import product22 from '../Assets/product22.jpg'
+import product23 from '../Assets/product23.jpg'
+import product24 from '../Assets/product24.jpg'
+import product25 from '../Assets/product25.jpg'
+import product26 from '../Assets/product26.jpg'
+import product27 from '../Assets/product27.jpg'
+import product28 from '../Assets/product28.jpg'
+import product29 from '../Assets/product29.jpg'
+import product30 from '../Assets/product30.jpg'
+import product31 from '../Assets/product31.jpg'
+import product32 from '../Assets/product32.jpg'
+import product33 from '../Assets/product33.jpg'
+let all_product=[
+    {
+        id:1,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"clothing",
+        image:Pic1,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:2,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"clothing",
+        image:Pic2,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:3,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"clothing",
+        image:Pic3,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:4,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"clothing",
+        image:Pic4,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:5,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"clothing",
+        image:Pic5,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:6,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"clothing",
+        image:Pic6,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:7,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"clothing",
+        image:Pic7,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:8,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"clothing",
+        image:Pic8,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:9,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"devices",
+        image:product2,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:10,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"devices",
+        image:product3,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:11,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"devices",
+        image:product4,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:12,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"devices",
+        image:product5,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:13,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"clothing",
+        image:product6,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:14,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"home",
+        image:product7,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:15,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"home",
+        image:product8,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:16,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"clothing",
+        image:product9,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:17,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"devices",
+        image:product10,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:18,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"devices",
+        image:product11,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:19,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"devices",
+        image:product12,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:20,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"clothing",
+        image:product13,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:21,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"clothing",
+        image:product14,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:22,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"home",
+        image:product15,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:23,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"home",
+        image:product16,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:24,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"clothing",
+        image:product17,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:25,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"devices",
+        image:product18,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:26,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"devices",
+        image:product19,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:27,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"devices",
+        image:product20,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:28,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"clothing",
+        image:product21,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:29,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"clothing",
+        image:product22,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:30,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"home",
+        image:product23,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:31,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"clothing",
+        image:product24,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:32,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"devices",
+        image:product25,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:33,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"devices",
+        image:product26,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:34,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"devices",
+        image:product27,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:35,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"clothing",
+        image:product28,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:36,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"home",
+        image:product29,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:37,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"home",
+        image:product30,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:38,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"clothing",
+        image:product31,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:39,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"clothing",
+        image:product32,
+        new_price:50.00,
+        old_price:80.00
+    },
+    {
+        id:40,
+        name:"blue and white full sleeve shirt with blue jeans",
+        category:"clothing",
+        image:product33,
+        new_price:50.00,
+        old_price:80.00
+    },
+]
+export default all_product;
