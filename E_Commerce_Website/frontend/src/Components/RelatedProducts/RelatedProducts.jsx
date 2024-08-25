8ed2f8a5556909f102f19603820ef7b25ecc1598
Muhammad Hassan Ghauri <94550154+HassanGhauri@users.noreply.github.com>
2024-08-25 14:28:55 +0500

    Update RelatedProducts.jsx

--- E_Commerce_Website/frontend/src/Components/RelatedProducts/RelatedProducts.jsx
+++ E_Commerce_Website/frontend/src/Components/RelatedProducts/RelatedProducts.jsx
@@ -1,17 +1,28 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './RelatedProducts.css'
-import data from '../Assets/data'
 import Item from '../Item/Item'
-const RelatedProducts = () => {
+import { ShopContext } from '../../Context/ShopContext'
+import axios from 'axios'
+const RelatedProducts = ({category}) => {
+  const[related_products,setRelated_Product] = useState([]);
+
+  useEffect(()=>{
+    axios.post("http://localhost:4000/relatedproducts",{category})
+    .then((data)=>{
+      setRelated_Product(data.data);
+    })
+  },[])
   return (
     <div className='relatedproducts'>
         <h1>Related Products</h1>
         <hr/>
         <div className='relatedproducts-item'>
-            {data.map((item,i)=>{
-                return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
-            })}
+        {related_products.map((item,i)=>{
+            return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+          } 
+        )}
         </div>
     </div>
   )
