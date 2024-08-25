fc678c102093ca21357de50a0d006d8d9735a69d
Muhammad Hassan Ghauri <94550154+HassanGhauri@users.noreply.github.com>
2024-08-25 15:12:33 +0500

    Update Navbar.jsx

--- E_Commerce_Website/frontend/src/Components/Navbar/Navbar.jsx
+++ E_Commerce_Website/frontend/src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,27 @@
 /* eslint-disable no-unused-vars */
-import React,{useContext, useState} from 'react'
+import React,{useContext, useRef, useState} from 'react'
 import './Navbar.css'
 import { FaShoppingCart } from "react-icons/fa";
 import { FaShopify } from "react-icons/fa6";
 import { Link } from 'react-router-dom';
 import { ShopContext } from '../../Context/ShopContext';
+import image from '../Assets/chevron.png';
 const Navbar = () => {
     const [menu,setMenu] = useState("shop");
     const {getTotalCartItems} = useContext(ShopContext);
+    const menuRef = useRef();
+    const dropdown_toggle = (e)=>{
+        menuRef.current.classList.toggle('nav-menu-visible');
+        e.target.classList.toggle('open');
+    }
   return (
     <div className='navbar'>
         <div className='nav-logo'>
             <span><FaShopify/></span>
             <p>Shop Inn</p>
         </div>
-        <ul className='nav-menu'>
+        <img src={image} alt='' className='nav-dropdown' onClick={dropdown_toggle}/>
+        <ul ref={menuRef} className='nav-menu'>
             <li onClick={()=>{setMenu("shop")}}><Link to='/' style={{textDecoration:'none'}}>Shop {menu==="shop" ? <hr/>:<></>}</Link></li>
             <li onClick={()=>{setMenu("clothing")}}><Link to='clothing' style={{textDecoration:'none'}}>Clothing & Fashion {menu==="clothing" ? <hr/>:<></>}</Link>  </li>
             <li onClick={()=>{setMenu("devices")}}><Link to='devices' style={{textDecoration:'none'}}>Electronic Devices {menu==="devices" ? <hr/>:<></>}</Link></li>
