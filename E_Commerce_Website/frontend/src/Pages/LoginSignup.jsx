940dd48cba6fbaa161da6fe1a6d158b25fe87363
Muhammad Hassan Ghauri <94550154+HassanGhauri@users.noreply.github.com>
2024-08-25 14:25:05 +0500

    Update LoginSignup.jsx

--- E_Commerce_Website/frontend/src/Pages/LoginSignup.jsx
+++ E_Commerce_Website/frontend/src/Pages/LoginSignup.jsx
@@ -63,10 +63,6 @@ const LoginSignup = () => {
         <button onClick={()=>{state === "Login"?login():signup()}}>Continue</button>
         {state==="Sign Up"?<p className='loginsignup-login'>Already have an account? <span onClick={()=>{setState("Login")}}>Login here</span> </p>
         :<p className='loginsignup-login'>Create an account? <span  onClick={()=>{setState("Sign Up")}}>Click here</span> </p>}
-        <div className='loginsignup-agree'>
-          <input type='checkbox' name='' id='' />
-          <p>By continuing, i agree to the terms of use & privacy policy </p>
-        </div>
       </div>
     </div>
   )
