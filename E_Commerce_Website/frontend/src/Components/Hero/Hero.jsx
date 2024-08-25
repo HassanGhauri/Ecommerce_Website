8ab1afbb6fd0ca0cce87db11b93e7a2a7aef3125
Muhammad Hassan Ghauri <94550154+HassanGhauri@users.noreply.github.com>
2024-08-25 15:13:57 +0500

    Update Hero.jsx

--- E_Commerce_Website/frontend/src/Components/Hero/Hero.jsx
+++ E_Commerce_Website/frontend/src/Components/Hero/Hero.jsx
@@ -1,30 +1,37 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import './Hero.css'
-import { PiHandWaving } from "react-icons/pi";
+
 import { FaArrowAltCircleRight } from "react-icons/fa";
 import hero_img from '../Assets/hero_img.png'
 const Hero = () => {
+    const clickhandler = ()=>{
+        window.scrollTo(500,2100)
+    }
   return (
     <div className='hero'>
+        <div className='hero-right'>
+            <img src={hero_img} alt='hero_img' />
+        </div>
         <div className='hero-left'>
-            <h2>New Arrivals Only</h2>
             <div>
                 <div className='hero-hand-icon'>
-                    <p>new</p>
-                    <span><PiHandWaving/></span>
+                    <p>Welcome</p>
+                    <span></span>
                 </div>
-                <p>collections</p>
-                <p>for everyone</p>
+                <p>To ShopInn!</p>
+                <p></p>
             </div>
-            <div className='hero-latest-btn'>
+            <h3>We offer products ranging from:</h3>
+            <h3>Clothing & Fashion to Home & lifstyle</h3>
+            <h3>Feel free to explore our e-store to your heart's liking.</h3>
+            <div className='hero-latest-btn' onClick={clickhandler}>
                 <div>Latest Collection</div>
                 <span><FaArrowAltCircleRight/></span>
             </div>
         </div>
-        <div className='hero-right'>
-            <img src={hero_img} alt='hero_img' />
-        </div>
+        
     </div>
   )
 }
